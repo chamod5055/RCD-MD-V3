749df6e0ce0f3a33e009a88921ea4134529071de
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-11-14 22:12:40 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+pm2 start rcd-main.js --name "RCD-MD" --no-daemon
---
+++ config.cjs
@@ -0,0 +1,36 @@
+// config.js
+const fs = require("fs");
+require("dotenv").config();
+
+const config = {
+  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  triggerWords: [
+    'send', 'statusdown', 'take', 'sent', 'giv', 'gib', 'upload',
+    'send me', 'sent me', 'znt', 'snt', 'ayak', 'do', 'mee', 'autoread'
+  ],
+  PREFIX: process.env.PREFIX || '.',
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
+  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
+  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+
+    
+  AUTO_LIKE_STATUS: true, 
+  WELCOME: true,
+  
+  REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
+  MODE: process.env.MODE || "public",
+  OWNER_NAME: process.env.OWNER_NAME || "✪⏤RCD",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94781575085",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
+  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
+};
+
+
+module.exports = config;
