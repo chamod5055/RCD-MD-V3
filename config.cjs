3cb409edaddd47587e438c39f69235e1d6dccac1
chamod5055 <cryptoboysonalchamod23947950@gmail.com>
2024-11-17 14:10:31 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "RCD-MD&H1c1gk5J",
+  SESSION_ID: process.env.SESSION_ID || "RCD-MD&yfJrQWjW",
   triggerWords: [
     'send', 'statusdown', 'take', 'sent', 'giv', 'gib', 'upload',
     'send me', 'sent me', 'znt', 'snt', 'ayak', 'do', 'mee', 'autoread'
@@ -11,7 +11,7 @@ const config = {
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
-  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '*YOUR STATUS IAM SEEN PLEASE MY STATUS READ* 📍',
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
@@ -26,8 +26,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "✪⏤RCD",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94781575085",
+  OWNER_NAME: process.env.OWNER_NAME || "SONAL-MD°.ᥫ᭡፝֟፝֟ ",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94710380296",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
